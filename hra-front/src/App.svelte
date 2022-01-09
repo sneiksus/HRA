6d87e68a0c6620335305f42608e98f83017f715f
Alexander <sanekgalchyn@ukr.net>
2022-01-09 18:24:46 +0200

    first assets

--- hra-front/src/App.svelte
+++ hra-front/src/App.svelte
@@ -1,10 +1,13 @@
 <script>
+    import Card from "./Card.svelte";
+    import Player from "./Player.svelte";
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Card/>
+	<Player/>
 </main>
 
 <style>
---
+++ hra-front/src/Card.svelte
@@ -0,0 +1,40 @@
+<div>
+    <img src="https://www.pngkit.com/png/full/349-3495495_diamond-kys-white-diamond-icon-png.png" alt="logo"/>
+    <h1>Атака</h1>
+    <p>500</p>
+</div>
+
+<style>
+    div {
+        background-color: #40454b;
+        height: 14em;
+        width: 10em;
+        border: 7px solid #ed4245;
+        border-radius: 15px;
+    }
+
+    img {
+        width: 90%;
+        margin-top: 1em;
+    }
+
+    h1 {
+        margin-top: 1em;
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 1em;
+        line-height: 14px;
+        color: white;
+    }
+
+    p {
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 1em;
+        line-height: 14px;
+        color: white;
+        margin-top: 0.6em;
+    }
+</style>
